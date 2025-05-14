20a629b64fd233b06ee1f6fd34b9d33b3bd81206
Sreekantha Bhargavi <144715760+bhargavi290504@users.noreply.github.com>
2025-05-14 12:21:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -244,4 +244,4 @@ if __name__ == '__main__':
         db.create_all()
     port = int(os.environ.get('PORT', 5000))  # Use Render's PORT or default to 5000
     app.run(host='0.0.0.0', port=port, debug=False) 
-)
+
